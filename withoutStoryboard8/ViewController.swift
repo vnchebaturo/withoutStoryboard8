9b8a23b00ede6a8de88dfd2481c5a986e5141afb
Chevaturo Vladimir Nikolaevich <winipuskin777@icloud.com>
2021-10-07 16:30:58 +0300

    Initial Commit

---
+++ withoutStoryboard8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  withoutStoryboard8
+//
+//  Created by Nikita on 07.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
